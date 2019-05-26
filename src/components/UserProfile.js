a4d0de324f51cff33df167e8d82ab846c7640ea7
Dino Tudor <dinotudor@gmail.com>
2019-05-26 19:05:09 +0200

    add favorite service Not working

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import profile from '../lib/profile-service';
+import {withAuth} from './../lib/AuthProvider';
 import { Link } from 'react-router-dom';
+import favorite from './../lib/favorite-service';
 
 class UserProfile extends Component {
   state = {
@@ -16,8 +18,22 @@ class UserProfile extends Component {
       this.setState({profile})
     })
   }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('FAVORITE ID -> ', this.props.match.params)
+    console.log('USER ID -> ', this.props.user._id)
+    const { userId } = this.props.user._id;
+    const { favoriteId } = this.props.match.params;
+
+    favorite.addFavorite(userId, favoriteId)
+    this.setState(userId, favoriteId)
+
+  }
+
   render() {
     const {profile} = this.state;
+    console.log('USER ID', profile)
     return (
       <div>
         <Link to='/dashboard'><button >Home</button></Link>
@@ -27,13 +43,15 @@ class UserProfile extends Component {
         <p>{profile.description}</p>
         <h4>instruments</h4>
         <p>{profile.instruments}</p>
-        <h4>Likes</h4>
+        <h4>Genres</h4>
         <p>{profile.genres}</p>
-        <Link to="/addFavorites"><button>Add favorite</button></Link>
+        <form onSubmit={this.handleFormSubmit}>
+          <input type="submit" value="Add Favorite" />
+        </form>
 
       </div>
     )
   }
 }
 
-export default UserProfile;
+export default withAuth(UserProfile);
--- src/lib/favorite-service.js
+++ src/lib/favorite-service.js
@@ -3,17 +3,17 @@ import axios from "axios";
 class Favorite {
   constructor() {
     this.favorite = axios.create({
-      baseURL: "http://localhost:5000/favorite",
+      baseURL: "http://localhost:5000/favorites",
       withCredentials: true
     });
   }
 
   getFavorites(id) {
-    return this.profile.get(`/${id}`).then(response => response.data);
+    return this.favorite.get(`/${id}`).then(response => response.data);
   }
 
-  addFavorite(type, url, title, description, year, month, id) {
-    return this.favorite.post("/", {type, url, title, description, year, month, id}).then(response => response.data);
+  addFavorite(userId, favoriteId) {
+    return this.favorite.post("/", userId, favoriteId ).then(response => response.data);
   }
 
 }
